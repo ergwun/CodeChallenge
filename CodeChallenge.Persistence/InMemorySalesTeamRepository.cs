de1f3605d210150f539c3052c3c7346cbdd59087
Geoff Battye <geoff.battye@gmail.com>
2020-11-09 20:21:33 +1100

    Added temporary in-memory repository.

---
+++ CodeChallenge.Persistence/InMemorySalesTeamRepository.cs
@@ -0,0 +1,22 @@
+﻿using CodeChallenge.Domain;
+using System;
+
+namespace CodeChallenge.Persistence
+{
+    /// <summary>
+    /// Temporary repository implementation that does not correctly manage concurrency.
+    /// </summary>
+    public class InMemorySalesTeamRepository : ISalesTeamRepository
+    {
+        private SalesTeam salesTeam;
+    
+        public InMemorySalesTeamRepository(SalesTeam initialSaleTeam)
+        {
+            this.salesTeam = initialSaleTeam;
+        }
+
+        public SalesTeam Get() => this.salesTeam;
+
+        public void Save(SalesTeam salesTeam) => this.salesTeam = salesTeam;
+    }
+}
